9d35fce095ad200f01ecf344f41aa11e4fa7fd8a
sahil.kumar6 <sahil.kumar@redbasil.in>
2024-02-02 11:48:49 +0530

    updated

--- app/(home)/preorder/page.tsx
+++ app/(home)/preorder/page.tsx
@@ -27,7 +27,7 @@ const page = () => {
                 <div className='p-4 mx-auto text-center'>
                     <p className='text-[37px] md:text-[40px] font-semibold text-[#FFD5A4]'>Key Features</p>
                     <div className='flex flex-wrap justify-evenly lg:mt-[82.17px] mt-[47px] '>
-                        <div className='flex flex-col items-center max-w-[17rem] mb-[25px] gap-4'>
+                        <div className='flex flex-col items-center mb-[25px] gap-4'>
                             <Image
                                 src="/Group1000006699.png"
                                 alt="picture"
@@ -35,19 +35,19 @@ const page = () => {
                                 height={103.7}
                             />
                             <p className='lg:w-[251px] w-[332px] lg:text-[25px] text-[20px] font-semibold mt-4'>Sacred Vedic Devotion Hub</p>
-                            <p className='font-light text-[15px] lg:text-[19px] text-center max-w-[274px] font-sans'>Dive into distraction-free devotion and access the essence of the Vedas, nurturing your spiritual journey.</p>
+                            <p className='font-light text-[15px] lg:text-[25px] text-center max-w-[274px] font-sans'>Dive into distraction-free devotion and access the essence of the Vedas, nurturing your spiritual journey.</p>
                         </div>
-                        <div className='flex flex-col items-center max-w-[13rem] mb-[25px] gap-4'>
+                        <div className='flex flex-col items-center mb-[25px] gap-4 '>
                             <Image
                                 src="/Group1000006701.png"
                                 alt="picture"
                                 width={62.08}
                                 height={116.12}
                             />
-                            <p className='lg:w-[251px] w-[282px] lg:text-[25px] text-[19px] font-semibold mt-4'>Flagship Sadhana App</p>
-                            <p className='font-light text-[15px] lg:text-[17px] text-center max-w-[326px] font-sans'>Interact with our revolutionary App for interactive Vedic worship, ad-free, subscription-free, and accessible to all.</p>
+                            <p className='lg:w-[205px] w-[282px] lg:text-[25px] text-[19px] font-semibold lg:mt-0'>Flagship Sadhana App</p>
+                            <p className='font-light text-[15px] lg:text-[25px] text-center max-w-[326px] font-sans'>Interact with our revolutionary App for interactive Vedic worship, ad-free, subscription-free, and accessible to all.</p>
                         </div>
-                        <div className='flex flex-col items-center max-w-[17rem] gap-4'>
+                        <div className='flex flex-col items-center gap-4'>
                             <Image
                                 src="/Group1000006700.png"
                                 alt="picture"
@@ -55,7 +55,7 @@ const page = () => {
                                 height={104.12}
                             />
                             <p className='lg:w-[251px] w-[332px] lg:text-[25px] text-[20px] font-semibold mt-4'>Curated Spiritual Content</p>
-                            <p className='font-light text-[15px] lg:text-[20px] text-center max-w-[334px] font-sans'>Handpicked spiritual books licensed from renowned authors, podcasts, soulful music, and guided meditation courses.</p>
+                            <p className='font-light text-[15px] lg:text-[25px] text-center max-w-[281px] lg:max-w-[324px] font-sans'>Handpicked spiritual books licensed from renowned authors, podcasts, soulful music, and guided meditation courses.</p>
                         </div>
                     </div>
                 </div>
@@ -72,42 +72,42 @@ const page = () => {
                     height={546}
                     className='m-auto'
                 />
-                <div className='flex flex-wrap justify-evenly items-center mt-8'>
-                    <div className='flex text-center gap-4 lg:flex-row flex-col'>
+                <div className='flex lg:flex-wrap md:flex-wrap justify-evenly items-start mt-8 gap-0 md:gap-[2rem] lg:gap-0'>
+                    <div className='flex text-center gap-4 lg:flex-row flex-col md:flex-row md:justify-center '>
                         <Image
                             src="/Group1000006703.png"
                             alt="picture"
                             width={73}
                             height={73.1}
-                            className='m-auto'
+                            className='m-auto w-[40px] md:w-[73px] lg:w-[73px] lg:h-[73.1px] h-[40px] md:h-[73.1px]'
                         />
-                        <p className='max-w-[19rem] font-semibold text-[15px] lg:text-[25px] mb-[25px]'>A 10-inch ultraportable model with a spacious liquid retina display.</p>
+                        <p className='max-w-[19rem] lg:max-w-[19rem] md:max-w-[11rem] font-semibold text-[15px] lg:text-[25px] '>A 10-inch ultraportable model with a spacious liquid retina display.</p>
                     </div>
-                    <div className='flex text-center gap-4 lg:flex-row flex-col'>
+                    <div className='flex text-center gap-4 lg:flex-row flex-col md:flex-row md:justify-center'>
                         <Image
                             src="/Group1000006702.png"
                             alt="picture"
                             width={53}
                             height={71.66}
-                            className='m-auto'
+                            className='m-auto w-[28px] md:w-[53px] lg:w-[53px] lg:h-[71.66px] h-[40px] md:h-[71.66px]'
                         />
-                        <p className='max-w-[16rem] font-semibold text-[15px] lg:text-[25px] mb-[25px]'>Supercharged by the xx chip, with battery life up to 18 hours.</p>
+                        <p className='max-w-[16rem] lg:max-w-[16rem] md:max-w-[10rem] font-semibold text-[15px] lg:text-[25px]'>Supercharged by the xx chip, with battery life up to 18 hours.</p>
                     </div>
 
-                    <div className='flex gap-4 lg:w-full lg:text-center lg:mt-[50px] justify-center  items-center text-center lg:flex-row flex-col '>
+                    <div className='flex gap-4 lg:w-full lg:text-center lg:mt-[50px] justify-center  items-center text-center lg:flex-row flex-col md:flex-row md:justify-center'>
                         <Image
                             src="/Vector.png"
                             alt="picture"
                             width={76}
                             height={62}
-                            className='h-[62px]'/>
-                        <p className='lg:max-w-[22rem] max-w-[19rem] font-semibold text-[15px] lg:text-[25px] lg:mt-4 '>Blazing fast performance, latest Bluetooth technology and enhanced augmented reality.</p>
+                            className='w-[40px] md:w-[76px] lg:w-[76px] lg:h-[62px] h-[33px] md:h-[62px] mb-[10px]'/>
+                        <p className='lg:max-w-[25rem] max-w-[19rem] md:max-w-[16rem] font-semibold text-[15px] lg:text-[25px] lg:mt-4 '>Blazing fast performance, latest Bluetooth technology and enhanced augmented reality.</p>
                     </div>
                 </div>
                 <br />
-                <h1 className='lg:text-[40px] text-[37px] font-semibold text-center text-[#ABE1FF] mt-[103px]'>Pro Benefits</h1>
+                <h1 className='lg:text-[40px] text-[37px] font-semibold text-center text-[#ABE1FF] mt-[64px] lg:mt-[103px] md:mt-[103px]'>Pro Benefits</h1>
                 <div className='flex mt-8 justify-evenly lg:flex-row md:flex-row flex-col p-2'>
-                    <div className='flex gap-4 p-4'>
+                    <div className='flex gap-4 p-4 flex-col md:flex-row lg:flex-row'>
                         <Image
                             src="/Group.png"
                             alt="picture"
@@ -122,7 +122,7 @@ const page = () => {
                         </div>
                     </div>
                     <hr className='text-white border-[1px] lg:h-[200px] md:h-[140px] ' />
-                    <div className='flex gap-4  p-4'>
+                    <div className='flex gap-4  p-4 flex-col md:flex-row lg:flex-row mt-[48px] lg:mt-[0px] md:mt-[0px]'>
                         <Image
                             src="/Group(1).png"
                             alt="picture"
@@ -145,7 +145,7 @@ const page = () => {
                         <source src="/Galaxy.mp4" type="video/mp4" />
                     </video>
                 </div>
-                <div className='w-full max-w-[1011px] m-auto mt-[112px] flex flex-col justify-flex-start items-center gap-4 px-4 sm:px-6 lg:px-8'>
+                <div className='w-full max-w-[1011px] m-auto mt-[57px] lg:mt-[112px] md:mt-[112px] flex flex-col justify-flex-start items-center gap-4 px-4 sm:px-6 lg:px-8'>
                     <p className='font-semibold text-center text-[#FFD5A4] lg:text-[25px] text-[15px] lg:w-[105%] w-full md:w-[662px]'>
                         Unlock a Wealth of Spiritual Wisdom featuring a treasure trove of spiritual wisdom, including interactive Vedic rituals, scriptures, e-books, podcasts, music and more.
                     </p>
@@ -176,10 +176,12 @@ const page = () => {
                             <div
                                 className="invisible h-auto max-h-0 items-center opacity-0 transition-all group-focus:visible group-focus:max-h-screen group-focus:opacity-100 group-focus:duration-1000"
                             >
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                commodo consequat.
+                                <ul className="list-disc ml-4">
+                                    <li>CPU: Octa-core</li>
+                                    <li>LCD: 10.36" 2000x1200 FHD</li>
+                                    <li>Memory: 6GB RAM</li>
+                                    <li>Custom-built apps, including Unity Gaming Engine, to cater to individual needs</li>
+                                </ul>
                             </div>
                         </div>
                         <div
@@ -196,10 +198,7 @@ const page = () => {
                             <div
                                 className="invisible h-auto max-h-0 items-center opacity-0 transition-all group-focus:visible group-focus:max-h-screen group-focus:opacity-100 group-focus:duration-1000"
                             >
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                commodo consequat.
+                                We respond back to your request in 24 hours.
                             </div>
                         </div>
                         <div
@@ -216,10 +215,7 @@ const page = () => {
                             <div
                                 className="invisible h-auto max-h-0 items-center opacity-0 transition-all group-focus:visible group-focus:max-h-screen group-focus:opacity-100 group-focus:duration-1000"
                             >
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                commodo consequat.
+                                Replacement warranty for 12 months
                             </div>
                         </div>
                         <div
